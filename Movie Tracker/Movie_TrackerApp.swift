ab47df1ebc5fd8f6240d1557dde4ead7f8c84a79
Shrinidhi Hegde <shrinidhi.hegde@capgemini.com>
2023-08-22 09:55:27 +0530

    Initial Commit

---
+++ Movie Tracker/Movie_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TrackerApp.swift
+//  Movie Tracker
+//
+//  Created by Shrinidhi Hegde on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
